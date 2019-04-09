954771b332134298a7796d2cdd5945993f622181
José Sousa <jose.sousa@farfetch.com>
2019-04-09 13:29:50 +0100

    baseic project structure

---
+++ kudos-slackbot/Data.Repository/Base/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Repository.Base
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAsync(params object[] args);
+
+        Task InsertAsync(T entity);
+    }
+}
