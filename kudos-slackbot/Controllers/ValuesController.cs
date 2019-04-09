16018333e29da0d445520a3fadb60487d5b08cb2
José Sousa <jose.sousa@farfetch.com>
2019-04-09 10:18:13 +0100

    Add project files.

---
+++ kudos-slackbot/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿namespace KudosSlackbot.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KudosController : ControllerBase
+    {
+        [HttpPost,
+        ProducesResponseType(204),
+        Route("")]
+        public async Task<IActionResult> CreateKudo(dynamic request)
+        {
+            return Ok(request);
+        }
+    }
+}
