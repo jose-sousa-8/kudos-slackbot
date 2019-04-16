4406dad616a30c56cce8214766462b15edc5399f
José Sousa <jose.sousa@farfetch.com>
2019-04-16 17:58:10 +0100

    Basic swagger settings

---
+++ kudos-slackbot/Presentation/Presentation.Api/Startup.Swagger.cs
@@ -0,0 +1,90 @@
+﻿namespace KudosSlackbot
+{
+    using System.IO;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.PlatformAbstractions;
+
+    using Swashbuckle.AspNetCore.Swagger;
+    using Swashbuckle.AspNetCore.SwaggerUI;
+
+    public partial class Startup
+    {
+        /// <summary>
+        /// Configures the swagger.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        public void ConfigureSwagger(IServiceCollection services)
+        {
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", GetApiInfo("v1"));
+
+                var filePath = Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "kudos-slackbot.xml");
+                if (File.Exists(filePath))
+                {
+                    c.IncludeXmlComments(filePath);
+                }
+
+                //if (Settings.EnableAuthentication)
+                //{
+                //    var identityUrl = Settings.IdentityServiceUrl;
+                //    c.AddSecurityDefinition("client_credentials", new OAuth2Scheme
+                //    {
+                //        Flow = "application",
+                //        AuthorizationUrl = identityUrl + "connect/authorize",
+                //        TokenUrl = identityUrl + "connect/token",
+                //        Scopes = new Dictionary<string, string>
+                //    {
+                //            { Settings.RequiredScope, "Access to Kudos Slack bot" },
+                //    }
+                //    });
+
+                //    // default authorization flow
+                //    var security = new Dictionary<string, IEnumerable<string>>
+                //    {
+                //            { "client_credentials", new string[] { Settings.RequiredScope } },
+                //    };
+                //    c.AddSecurityRequirement(security);
+
+                //    c.OperationFilter<MakeFarfetchOperation>();
+                //    c.DocumentFilter<MakeFarfetchDocument>();
+                //}
+            });
+        }
+
+        /// <summary>
+        /// Configures the swagger.
+        /// </summary>
+        /// <param name="app">The application.</param>
+        public void ConfigureSwagger(IApplicationBuilder app)
+        {
+            app.UseSwagger(c => { c.RouteTemplate = "{documentName}/swagger.json"; });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/v1/swagger.json", "KUDOS Slackbot");
+                c.SupportedSubmitMethods(new[] { SubmitMethod.Get, SubmitMethod.Post, SubmitMethod.Put, SubmitMethod.Patch, SubmitMethod.Delete });
+                c.RoutePrefix = "swagger/api";
+                c.EnableValidator(null);
+            });
+        }
+
+        private Info GetApiInfo(string version)
+        {
+            return new Info
+            {
+                Version = version,
+                Title = "KUDOS Slackbot",
+                Description = "Kudos slack bot API.",
+                Contact = new Contact
+                {
+                    Name = "José Sousa",
+                    Email = "zpedro_s@hotmail.com"
+                }
+            };
+        }
+    }
+}
