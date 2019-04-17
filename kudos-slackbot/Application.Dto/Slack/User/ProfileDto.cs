0011c48170fb1820a21ef19ba541bb443987eea8
José Sousa <jose.sousa@farfetch.com>
2019-04-17 10:24:18 +0100

    Slack appsettings

---
+++ kudos-slackbot/Application.Dto/Slack/User/ProfileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KudosSlackbot.Application.Dto.Slack.User
+{
+    public class ProfileDto
+    {
+        public bool ok { get; set; }
+
+        public Profile profile { get; set; }
+    }
+}
---
+++ kudos-slackbot/Infrastructure/Infrastructure.CrossCutting/Settings/Slack/SlackCredentials.cs
@@ -0,0 +1,15 @@
+﻿namespace KudosSlackbot.Infrastructure.Settings.Slack
+{
+    public class SlackCredentials
+    {
+        public string AppId { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string SigningSecret { get; set; }
+
+        public string VerificationToken { get; set; }
+    }
+}
