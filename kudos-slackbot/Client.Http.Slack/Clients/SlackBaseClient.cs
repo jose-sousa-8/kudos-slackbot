b1f488d8391013af59a658f0b885124286940aa9
José Sousa <jose.sousa@farfetch.com>
2019-04-23 14:33:11 +0100

    refactored http response processing

--- kudos-slackbot/Client.Http.Slack/Clients/SlackBaseClient.cs
+++ kudos-slackbot/Client.Http.Slack/Clients/SlackBaseClient.cs
@@ -1,6 +1,11 @@
 ﻿namespace KudosSlackbot.Client.Http.Slack.Clients
 {
     using System;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Newtonsoft.Json;
 
     public abstract class SlackBaseClient
     {
@@ -13,6 +18,15 @@
         public SlackBaseClient(string slackApiEndpoint, string slackMethod)
         {
             this.SlackApiUri = new Uri(string.Format("{0}{1}", slackApiEndpoint, slackMethod));
+        }
+
+        protected virtual async Task<T> ProcessResponse<T>(HttpResponseMessage response)
+        {
+            var byteArr = await response.Content.ReadAsByteArrayAsync();
+
+            var json = Encoding.UTF8.GetString(byteArr);
+
+            return JsonConvert.DeserializeObject<T>(json);
         }
     }
 }
