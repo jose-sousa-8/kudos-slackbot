65140c7b6c457ae02aef45d770ec22207b318a53
José Sousa <jose.sousa@farfetch.com>
2019-04-23 14:33:11 +0100

    refactored http response processing

--- kudos-slackbot/Client.Http.Slack/Clients/SlackUsersClient.cs
+++ kudos-slackbot/Client.Http.Slack/Clients/SlackUsersClient.cs
@@ -3,12 +3,9 @@
     using System;
     using System.Collections.Generic;
     using System.Net.Http;
-    using System.Text;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.WebUtilities;
-
-    using Newtonsoft.Json;
 
     public class SlackUsersClient : SlackBaseClient, ISlackUsersClient
     {
@@ -17,7 +14,6 @@
         public SlackUsersClient(string slackApiEndpoint) : base(slackApiEndpoint, Method)
         {
         }
-
 
         public async Task<UserInfoSlackHttpResponse> GetUserInfo(string userId, bool includeLocal = false)
         {
@@ -33,11 +29,7 @@
 
                     var response = await httpClient.GetAsync(QueryHelpers.AddQueryString(base.SlackApiUri.ToString(), queryParameters));
 
-                    var byteArr = await response.Content.ReadAsByteArrayAsync();
-
-                    var json = Encoding.UTF8.GetString(byteArr);
-
-                    return JsonConvert.DeserializeObject<UserInfoSlackHttpResponse>(json);
+                    return await base.ProcessResponse<UserInfoSlackHttpResponse>(response);
                 }
                 catch (Exception ex)
                 {
